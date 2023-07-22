381b0e856916838b14aabf411c46040c2e650252
erik-lance <eriklance@gmail.com>
2023-07-23 00:11:10 +0800

    Added initial hero

---
+++ src/app/components/Hero.tsx
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <div className="flex flex-col items-center justify-center">
+            <h1 className="text-6xl font-bold text-center text-white">
+                My name is Erik Lance Tiongquico.
+            </h1>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+import Hero from "./components/Hero"
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-6xl font-bold text-center">
         Welcome to <a href="https://nextjs.org">Next.js!</a>
+        <Hero />
       </h1>
     </main>
   )
