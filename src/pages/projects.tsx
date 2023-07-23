14cd55862db0f995b2f8768d6a4fdcd51cf675b2
erik-lance <eriklance@gmail.com>
2023-07-23 15:08:30 +0800

    Added initial grid

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,7 +1,28 @@
+import ProjectTile from "@/components/ProjectTile";
+import { Grid } from "@mui/material";
+
 export default function Projects() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            Test
-        </div>
+        <>
+            <h1 className="text-6xl font-bold text-center">Projects</h1>
+            <div>
+                <Grid
+                    container
+                    spacing={2}
+                >
+                    <Grid item xs>
+                        <ProjectTile
+                            name="Test"
+                            tagline="Test"
+                            description="Test"
+                            image="Test"
+                            link="Test"
+                            github="Test"
+                            tech={["Test"]}
+                        />
+                    </Grid>
+                </Grid>
+            </div>
+        </>
     );
 }
