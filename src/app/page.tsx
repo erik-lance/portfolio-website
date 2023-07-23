4f80c69130244c322ae3b04813977b8ff94db5e3
erik-lance <eriklance@gmail.com>
2023-07-23 14:22:37 +0800

    Added header to Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+import Header from "./components/Header"
 import Hero from "./components/Hero"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-6xl font-bold text-center">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
-        <Hero />
-      </h1>
+    <main className="flex flex-col min-h-screen justify-between">
+      <Header />
+      <div className="flex flex-col min-h-screen items-center justify-between p-24">
+        <h1 className="text-6xl font-bold text-center">
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          <Hero />
+        </h1>
+      </div>
     </main>
   )
 }
