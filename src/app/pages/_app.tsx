c25cf3d3af31d278debf7d5996cea561550f73b9
erik-lance <eriklance@gmail.com>
2023-07-23 14:22:28 +0800

    Added initial layout for pages

---
+++ src/app/pages/_app.tsx
@@ -0,0 +1,23 @@
+import '@/styles/globals.css'
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
+
+import type { AppProps } from 'next/app';
+import Header from '../components/Header';
+
+// Makes every page under pages/ use the same layout
+function MyApp({ Component, pageProps }: AppProps) {
+    return (
+        <div className={inter.className}>
+            <main className='flex flex-col min-h-screen justify-between'>
+                <Header />
+                <div className="flex flex-col items-center justify-between p-24">
+                    <Component {...pageProps} />
+                </div>
+            </main>
+        </div>
+    );
+}
+
+export default MyApp;
