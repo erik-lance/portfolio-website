593e86d14c8542cd8d3c0b2ee05e558b1796535c
erik-lance <eriklance@gmail.com>
2023-07-23 14:28:52 +0800

    Moved folders out of app

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -1,6 +1,7 @@
 export default function Projects() {
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
+            Test
         </div>
     );
 }
