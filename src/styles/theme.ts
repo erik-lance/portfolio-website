99c5cdbed3eb6a98855283ad0fb0eda3104a6c42
erik-lance <eriklance@gmail.com>
2023-07-24 23:30:01 +0800

    Added initial theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,15 @@ import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
     palette: {
+        primary: {
+            light: '#95C540',
+            main: '#427F3B',
+            dark: '#1F3736'
+        },
+        secondary: {
+            light: '#523B40',
+            main: '#3D253B',
+        },
         
     }
 });
