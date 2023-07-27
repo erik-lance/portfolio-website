721f12fb613b6ecc38df4208b8ac58ec52c22afa
erik-lance <eriklance@gmail.com>
2023-07-27 21:41:50 +0800

    Added Education and Certifications to navbar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,10 @@ export default function Header() {
         <div className='w-full flex flex-row justify-between items-center p-4'>
             <a className='text-2xl font-bold' href='/'>Home</a>
             <div className='flex flex-row space-x-4'>
+                <a href='education'>Education</a>
                 <a href='experience'>Experience</a>
                 <a href='projects'>Projects</a>
+                <a href='certifications'>Certifications</a>
                 <a href='contact'>Contact</a>
                 <a href='resume'>Resume</a>
             </div>
