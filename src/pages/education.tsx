40037bd8691dff0339230b13315fa7461295a4c3
erik-lance <eriklance@gmail.com>
2023-08-12 23:58:54 +0800

    Added initial education list

--- src/pages/education.tsx
+++ src/pages/education.tsx
@@ -1,4 +1,6 @@
-import { Container, Stack, Typography, List } from "@mui/material";
+import { Container, Stack, Typography, List, ListItem, IconButton } from "@mui/material";
+import SchoolIcon from '@mui/icons-material/School';
+import education from "@/data/education.json";
 
 export default function Education() {
     return (
@@ -21,7 +23,18 @@ export default function Education() {
                         width: '100%',
                     }}
                 >
+                    {education.map((school) => (
+                        <ListItem
+                            key={school.id}
+                            secondaryAction={
+                                <IconButton edge="end" aria-label="open school page">
+                                    <SchoolIcon />
+                                </IconButton>
+                            }
+                        >
 
+                        </ListItem>
+                    ))}
                 </List>
                 
             </Stack>
