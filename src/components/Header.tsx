d2a8409c5bc58f5dc43cfc5d3183f686926a9471
erik-lance <eriklance@gmail.com>
2023-08-12 23:14:48 +0800

    Fixed navbar links to comply with nextjs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
+
 export default function Header() {
     return (
         <div className='w-full flex flex-row justify-between items-center p-4'>
-            <a className='text-2xl font-bold' href='/'>Home</a>
+            <Link href='/' className="text-2sl font-bold">Home</Link>
             <div className='flex flex-row space-x-4'>
-                <a href='education'>Education</a>
-                <a href='experience'>Experience</a>
-                <a href='projects'>Projects</a>
-                <a href='certifications'>Certifications</a>
-                <a href='events'>Events</a>
-                <a href='contact'>Contact</a>
-                <a href='resume'>Resume</a>
+                <Link href='/education'>Education</Link>
+                <Link href='/experience'>Experience</Link>
+                <Link href='/projects'>Projects</Link>
+                <Link href='/certifications'>Certifications</Link>
+                <Link href='/events'>Events</Link>
+                <Link href='/contact'>Contact</Link>
+                <Link href='/resume'>Resume</Link>
             </div>
         </div>
     );
