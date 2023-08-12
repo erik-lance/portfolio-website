09da20abd6ac8e478a14d88e4e039dae7826b057
erik-lance <eriklance@gmail.com>
2023-08-12 23:16:00 +0800

    Fixed typo on tailwind text

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Header() {
     return (
         <div className='w-full flex flex-row justify-between items-center p-4'>
-            <Link href='/' className="text-2sl font-bold">Home</Link>
+            <Link href='/' className="text-2xl font-bold">Home</Link>
             <div className='flex flex-row space-x-4'>
                 <Link href='/education'>Education</Link>
                 <Link href='/experience'>Experience</Link>
