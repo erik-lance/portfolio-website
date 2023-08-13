27222b5d97965262ee992fa21409dc9b4f679256
erik-lance <eriklance@gmail.com>
2023-08-14 01:20:22 +0800

    Added iframe of pdf

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -3,8 +3,12 @@ import { Document } from "react-pdf";
 
 export default function Resume() {
     return (
-        <Container maxWidth="lg">
-            <Stack spacing={2}>
+        <Container maxWidth="lg"
+            sx={{
+                height: '100vh',
+            }}
+        >
+            <Stack spacing={2} height="100%">
                 <Typography
                     variant="h4"
                     fontWeight={700}
@@ -13,7 +17,19 @@ export default function Resume() {
                     Resume
                 </Typography>
 
-                <Paper elevation={5}>
+                <Paper 
+                    elevation={5}
+                    sx={{
+                        width: '100%',
+                        height: '100%',
+                    }}
+                >
+                    <iframe
+                        src="/documents/tiongquico_resume.pdf"
+                        width="100%"
+                        height="100%"
+                        style={{ border: "none" }}
+                    />
                 </Paper>
             </Stack>
         </Container>
