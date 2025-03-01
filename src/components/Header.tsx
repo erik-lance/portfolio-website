f36f3ee58efa5e69169220cd530cc0a01d5faccd
Erik Lance <73865149+erik-lance@users.noreply.github.com>
2025-03-01 12:42:25 +0800

    Updated certificate route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ export default function Header() {
                 <Link href='/education'>Education</Link>
                 <Link href='/experience'>Experience</Link>
                 <Link href='/projects'>Projects</Link>
-                <Link href='/certifications'>Certifications</Link>
+                <Link href='/certificates'>Certifications</Link>
                 <Link href='/events'>Events</Link>
                 <Link href='/resume'>Resume</Link>
             </div>
